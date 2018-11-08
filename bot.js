976acadec2d35ff6654b6d0e7062f2f6c91cd3c0
ZaLiV12 <44550056+ZaLiV12@users.noreply.github.com>
2018-11-08 18:48:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ليس كل سقؤط , نهآيه .. ف سقؤط آلستيآنآت آجمل بدآيه `,"http://twitch.tv/Death Shop")
+client.user.setGame(`ؤآذآ بنيه كسرتت كلبك آني بيدي آختم عمرهآ , مثل كلبك مآتكسره , آني رآح آكسر ظهرهآ  `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
